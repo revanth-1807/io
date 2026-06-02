7f8e34022ad299f5762b082bc52f9f8f7b860f4d
revanth-1807 <revanth1807r@gmail.com>
2026-06-02 21:04:32 +0530

    Add verified field to user schema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   Name: { type: String, required: true },
   Email: { type: String, required: true, unique: true },
   Password: { type: String, required: true },
-
+verified: { type: Boolean, default: false },
 })
 
 module.exports = mongoose.model('Userlogin', userSchema);
